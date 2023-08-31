4d689a9d36ccc4f29081ef7bb0c72973c8fa5e40
go2tom42 <tom42martin@gmail.com>
2023-08-31 11:29:00 -0400

    remove spill

--- runme.sh
+++ runme.sh
@@ -7,19 +7,14 @@ cp -fr "./blur117/EXTRA MODS/Circular homepage shortcuts/circular_homepage_short
 cp -fr "./blur117/EXTRA MODS/Compact extensions menu/Style 1/cleaner_extensions_menu.css" "./chrome/cleaner_extensions_menu.css"
 cp -fr "./blur117/EXTRA MODS/Icon mods/Icons in main menu/icons_in_main_menu.css" "./chrome/icons_in_main_menu.css"
 cp -fr "./blur117/EXTRA MODS/Min-max-close window buttons/Right side default system buttons/MenuButtonsOnLeft/min-max-close_buttons.css" "./chrome/min-max-close_buttons.css"
-cp -fr "./blur117/EXTRA MODS/Remove text from homepage shortcuts/remove_homepage_shortcut_title_text.css" "./chrome/remove_homepage_shortcut_title_text.css"
 cp -fr "./blur117/EXTRA MODS/Colored sound playing tab/colored_soundplaying_tab.css" "./chrome/colored_soundplaying_tab.css"
-cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part1-file.css" "./chrome/-spill-style-part1-file.css"
-cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part2-file.css" "./chrome/-spill-style-part2-file.css"
 cp -fr "./blur117/userChrome.css" "./chrome/old_userChrome.css"
 cp -fr "./blur117/userContent.css" "./chrome/old_userContent.css"
-
 cp -fr "./blur117/image" "./chrome/image"
 
 
 cp -fr "./nox/Multirow and other functions/JS Loader/root/defaults" "./Firefox Install Folder/defaults"
 cp -fr "./nox/Multirow and other functions/JS Loader/root/config.js" "./Firefox Install Folder/config.js"
-
 
 cp -fr "./nox/Multirow and other functions/JS Loader/utils" "./chrome/utils"
 cp -fr "./nox/Multirow and other functions/Multirow tabs/MultiRowTabLiteforFx.uc.js" "./chrome/MultiRowTabLiteforFx.uc.js"
