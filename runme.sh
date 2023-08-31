2f34515f3675019b0d658745b9fe4fe1ad71536c
go2tom42 <tom42martin@gmail.com>
2023-08-30 23:33:45 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 git submodule sync
 git submodule update --init --recursive
-cp -f "./blur117/EXTRA MODS/Circular homepage shortcuts/circular_homepage_shortcuts.css" "./chrome/circular_homepage_shortcuts.css"
-cp -f "./blur117/EXTRA MODS/Compact extensions menu/Style 1/cleaner_extensions_menu.css" "./chrome/cleaner_extensions_menu.css"
-cp -f "./blur117/EXTRA MODS/Icon mods/Icons in main menu/icons_in_main_menu.css" "./chrome/icons_in_main_menu.css"
-cp -f "./blur117/EXTRA MODS/Min-max-close window buttons/Right side default system buttons/MenuButtonsOnLeft/min-max-close_buttons.css" "./chrome/min-max-close_buttons.css"
-cp -f "./blur117/EXTRA MODS/Remove text from homepage shortcuts/remove_homepage_shortcut_title_text.css" "./chrome/remove_homepage_shortcut_title_text.css"
-cp -f "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part1-file.css" "./chrome/spill-style-part1-file.css"
-cp -f "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part2-file.css" "./chrome/spill-style-part2-file.css"
-cp -f "./blur117/userChrome.css" "./chrome/old_userChrome.css"
-cp -f "./blur117/userContent.css" "./chrome/old_userContent.css"
+cp -fr "./blur117/EXTRA MODS/Circular homepage shortcuts/circular_homepage_shortcuts.css" "./chrome/circular_homepage_shortcuts.css"
+cp -fr "./blur117/EXTRA MODS/Compact extensions menu/Style 1/cleaner_extensions_menu.css" "./chrome/cleaner_extensions_menu.css"
+cp -fr "./blur117/EXTRA MODS/Icon mods/Icons in main menu/icons_in_main_menu.css" "./chrome/icons_in_main_menu.css"
+cp -fr "./blur117/EXTRA MODS/Min-max-close window buttons/Right side default system buttons/MenuButtonsOnLeft/min-max-close_buttons.css" "./chrome/min-max-close_buttons.css"
+cp -fr "./blur117/EXTRA MODS/Remove text from homepage shortcuts/remove_homepage_shortcut_title_text.css" "./chrome/remove_homepage_shortcut_title_text.css"
+cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part1-file.css" "./chrome/spill-style-part1-file.css"
+cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part2-file.css" "./chrome/spill-style-part2-file.css"
+cp -fr "./blur117/userChrome.css" "./chrome/old_userChrome.css"
+cp -fr "./blur117/userContent.css" "./chrome/old_userContent.css"
+md "./chrome/image"
+cp -fr "./blur117/image" "./chrome/image"
