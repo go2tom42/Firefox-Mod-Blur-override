dfa2e74136f69a570850974d327e26fa5e7e16b5
go2tom42 <tom42martin@gmail.com>
2023-08-31 03:18:33 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -6,8 +6,6 @@ cp -fr "./blur117/EXTRA MODS/Compact extensions menu/Style 1/cleaner_extensions_
 cp -fr "./blur117/EXTRA MODS/Icon mods/Icons in main menu/icons_in_main_menu.css" "./chrome/icons_in_main_menu.css"
 cp -fr "./blur117/EXTRA MODS/Min-max-close window buttons/Right side default system buttons/MenuButtonsOnLeft/min-max-close_buttons.css" "./chrome/min-max-close_buttons.css"
 cp -fr "./blur117/EXTRA MODS/Remove text from homepage shortcuts/remove_homepage_shortcut_title_text.css" "./chrome/remove_homepage_shortcut_title_text.css"
-cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part1-file.css" "./chrome/spill-style-part1-file.css"
-cp -fr "./blur117/EXTRA THEMES/Spill Theme Files/spill-style-part2-file.css" "./chrome/spill-style-part2-file.css"
 cp -fr "./blur117/EXTRA MODS/Colored sound playing tab/colored_soundplaying_tab.css" "./chrome/colored_soundplaying_tab.css"
 cp -fr "./blur117/userChrome.css" "./chrome/old_userChrome.css"
 cp -fr "./blur117/userContent.css" "./chrome/old_userContent.css"
